0832c4476bb135a919c57726eaafa4f206cabc27
Tomasz Trzcinski <trzcinski.t@gmail.com>
2024-12-03 23:49:53 +0100

    Adds inverted relationship for the user in the chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Chirp extends Model
 {
     /**
@@ -12,4 +12,12 @@ class Chirp extends Model
      * @var array<int, string>
      */
     protected $fillable = ['message'];
+
+    /**
+     * Get the user that owns the chirp.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
