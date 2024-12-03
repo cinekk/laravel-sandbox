70963616cc835cf7cf5cab61e6e893355646a490
Tomasz Trzcinski <trzcinski.t@gmail.com>
2024-12-03 23:25:51 +0100

    Adds a Chirp adding form. Adds a handler to add the Chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['message'];
 }
--- resources/views/livewire/chirps/create.blade.php
+++ resources/views/livewire/chirps/create.blade.php
@@ -1,9 +1,20 @@
 <?php
 
 use Livewire\Volt\Component;
+use Livewire\Attributes\Validate;
 
 new class extends Component {
+    #[Validate('required|string|max:255')]
     public string $message = '';
+
+    public function create(): void
+    {
+        $validated = $this->validate();
+
+        auth()->user()->chirps()->create($validated);
+
+        $this->message = '';
+    }
 }; ?>
 
 <div>
@@ -11,6 +22,7 @@ new class extends Component {
         <textarea wire:model="message" placeholder="{{ __('What\'s on your mind?') }}"
             class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
         </textarea>
+        <small><span x-text="dupa"></span></small>
 
         <x-input-error :messages="$errors->get('message')" class="mt-2" />
         <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
