e5eb30ff41bebf6f516b7dbd17b98019c9ffa31b
Tomasz Trzcinski <trzcinski.t@gmail.com>
2024-12-03 23:12:54 +0100

    Initial commit

---
+++ resources/views/chirps.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Chirps') }}
+        </h2>
+    </x-slot>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <livewire:chirps.create />
+    </div>
+</x-app-layout>
